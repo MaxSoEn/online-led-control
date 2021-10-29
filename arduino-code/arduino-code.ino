e0247ec1cdf7103fb3044318c875eb22bf0d18de
TheMidgetWidget <lightlord323@gmail.com>
2021-10-29 19:07:38 +0200

    Improve arduino code

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -8,53 +8,41 @@ WebSocketsClient wsc;
 const char *ssid = "max";
 const char *pass = "123456789";
 
-#define SERVER  "192.168.43.170"
-#define PORT    80
-#define URL     "/"
+#define SERVER "192.168.43.170"
+#define PORT 80
+#define URL "/"
 #define redPin 32
 #define greenPin 33
 #define bluePin 25
 
-
-void websocketEvent(WStype_t type, uint8_t *data, size_t length){
-  switch(type){
-    case(WStype_CONNECTED):
-      Serial.printf("connected to server\n");
-      break;
-    case WStype_DISCONNECTED:
-      Serial.printf("Disconnected!\n");
-      break;
-    case(WStype_TEXT):
-      int fr = 0, fg = 0, fb = 0;
-      //Serial.printf("data: %s\n",data);
-      if(data[0] == 'r'){
-        for(byte i = 1; i < length; i++){
-            fr += (data[i] - '0') * pow(10, (length - 1) - (i));
-        }
-        int v = map(fr, 0, 255, 0, 1024);
-        analogWrite(redPin,v);
-        Serial.printf("Red fade: %d\n", fr);
-      }else if(data[0] == 'g'){
-        for(byte i = 1; i < length; i++){
-            fg += (data[i] - '0') * pow(10, (length - 1) - (i));
-        }
-        int v = map(fg, 0, 255, 0, 1024);
-        analogWrite(greenPin, v);
-        Serial.printf("green fade: %d\n", fg);
-      }else if(data[0] == 'b'){
-        for(byte i = 1; i < length; i++){
-            fb += (data[i] - '0') * pow(10, (length - 1) - (i));
-        }
-        int v = map(fb,0,255,0,1024);
-        analogWrite(bluePin,v);
-        Serial.printf("blue fade: %d\n", fb);
-      }
-      
-      break;
+void websocketEvent(WStype_t type, uint8_t *data, size_t length) {
+  switch (type) {
+  case (WStype_CONNECTED):
+    Serial.printf("connected to server\n");
+    break;
+  case WStype_DISCONNECTED:
+    Serial.printf("Disconnected!\n");
+    break;
+  case (WStype_TEXT):
+    int value = 0;
+    for (byte i = 1; i < length; i++) {
+        value += (data[i] - '0') * pow(10, (length - 1) - (i));
+    }
+    value = map(value, 0, 255, 0, 1023);
+    Serial.printf("%c fade: %d\n", data[0], value);
+    if (data[0] == 'r') {
+      analogWrite(redPin, value);
+    } else if (data[0] == 'g') {
+      analogWrite(greenPin, value);
+    } else {
+      analogWrite(bluePin, value);
+    }
+    break;
   }
 }
 
-void setup(){
+void setup()
+{
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
@@ -62,7 +50,8 @@ void setup(){
 
   WiFi.begin(ssid, pass);
   Serial.print("conecting");
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
@@ -76,6 +65,7 @@ void setup(){
   wsc.setReconnectInterval(1000);
 }
 
-void loop(){
+void loop()
+{
   wsc.loop();
 }
